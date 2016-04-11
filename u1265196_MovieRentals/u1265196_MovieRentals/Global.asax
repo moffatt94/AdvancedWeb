7f8ce09f8a185866ab41cf1566158a3794389d7f
james moffatt <moffatt123@hotmail.co.uk>
2016-04-11 14:26:29 +0100

    Initial Project Build

---
+++ u1265196_MovieRentals/u1265196_MovieRentals/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="u1265196_MovieRentals.MvcApplication" Language="C#" %>
---
+++ u1265196_MovieRentals/u1265196_MovieRentals/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(u1265196_MovieRentals.Startup))]
+namespace u1265196_MovieRentals
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
